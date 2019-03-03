df798d6bfa73cb802bc2495cc0622c691a3c8242
Rolan <miguelrolando123@gmail.com>
2019-03-02 18:18:24 -0600

    aprendiendo git

---
+++ juegodelavida.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+
+int main ()
+{
+
+	int columnas,filas;
+
+	printf("\nIngrese el número de columnas: ");
+	scanf("%d",&columnas);
+	printf("\nIngrese el número de filas: ");
+	scanf("%d",&filas);
+	 
+
+	//scanf()
+} 
